4df78f8e3570e9df049438b4bcf881c3ebfb2b4a
Carlos Sol <fersolmarin@hotmail.com>
2019-11-06 17:22:32 -0600

    Base AI Implemented

--- src/board/control/AI/makeMove.js
+++ src/board/control/AI/makeMove.js
@@ -1,1 +1,25 @@
-// AI Move Logic
+import currentTurn from '../gameLogic/currentTurn';
+
+let movements = [];
+const letters = 'abcdefghij'.split('');
+
+const getCol = () => Math.floor((Math.random() * 10) + 1);
+
+const getRow = () => letters[Math.floor(Math.random() * 10)];
+
+const getMovement = () => {
+  let movement = '';
+  do {
+    movement = `cell-${getCol()}-${getRow()}`
+  }
+  while (movements.includes(movement))
+  console.log(`Movement: ${movement}`);
+  return movement;
+};
+
+export default () => {
+  const movement = getMovement();
+  movements.push(movement);
+  console.log(movements);
+  currentTurn(false, movement);
+};
